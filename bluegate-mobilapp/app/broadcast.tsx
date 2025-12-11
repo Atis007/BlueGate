f75d8e88f675bfff104f8b296d255ea41721d0aa
Tóth Attila <tothattila5559@gmail.com>
2025-12-11 14:05:26 +0100

    Folyamatban lévő módosítások mentése a mobilapp és a Raspberry scanner fájljaiban (broadcast, configok, scan.py)

--- bluegate-mobilapp/app/broadcast.tsx
+++ bluegate-mobilapp/app/broadcast.tsx
@@ -1,6 +1,5 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-<<<<<<< HEAD
 import { Feather } from "@expo/vector-icons";
 import { router } from "expo-router";
 import * as ClipBoard from "expo-clipboard";
@@ -57,46 +56,11 @@ export default function Broadcast() {
                 color="#11dee8"
                 className="material-symbols-outlined"
               />
-=======
-import { router } from "expo-router";
-
-export default function Broadcast() {
-  return (
-    <SafeAreaView className="flex-1 bg-background-dark">
-      {/* Header */}
-      <View className="flex-row items-center px-4 pb-2 justify-between">
-        <View className="w-12 h-12" />
-        <Text className="text-lg font-bold text-text-light flex-1 text-center">
-          Irányítópult
-        </Text>
-        <TouchableOpacity
-          className="w-12 h-12 items-center justify-center"
-          onPress={() => router.push("/")}
-        >
-          <Text className="text-text-light/60 text-3xl">⊗</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* Content - Centered */}
-      <View className="flex-1 justify-center items-center px-4 -mt-16">
-        {/* Index Number Section */}
-        <View className="w-full max-w-sm items-center">
-          <Text className="text-primary/70 text-sm font-normal pb-1 pt-1">
-            Index Szám
-          </Text>
-          <View className="flex-row items-center justify-center gap-3">
-            <Text className="text-text-light text-4xl font-bold">
-              DUMMY INDEX
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-2xl">{/* Copy jel helye */}</Text>
->>>>>>> 68be985 (Tailwind css es NativeWind css inicializálása, egyedi dizájn rendszer beállítása, valamint az AGI-12es feladat route resze)
             </TouchableOpacity>
           </View>
         </View>
 
         {/* Spacer */}
-<<<<<<< HEAD
         <View className="h-10" />
 
         {/* Bluetooth ID Section */}
@@ -174,32 +138,6 @@ export default function Broadcast() {
                 </Text>
               </TouchableOpacity>
             </View>
-=======
-        <View className="h-16" />
-
-        {/* Bluetooth ID Section */}
-        <View className="w-full max-w-sm items-center">
-          <Text className="text-primary/70 text-sm font-normal pb-1 pt-1">
-            Bluetooth ID
-          </Text>
-          <View className="flex-row items-center justify-center gap-3">
-            <Text className="text-text-light text-4xl font-bold">
-              DUMMY-MAC-CIM
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-2xl">{/* Copy jel helye */}</Text>
-            </TouchableOpacity>
-          </View>
-
-          {/* Connect Button */}
-          <View className="mt-8">
-            <TouchableOpacity className="flex-row items-center justify-center gap-2 rounded-full bg-primary/20 px-6 py-3">
-              <Text className="text-xl">{/* Bluethoot jel helye */}</Text>
-              <Text className="text-lg font-bold text-primary">
-                Csatlakozás
-              </Text>
-            </TouchableOpacity>
->>>>>>> 68be985 (Tailwind css es NativeWind css inicializálása, egyedi dizájn rendszer beállítása, valamint az AGI-12es feladat route resze)
           </View>
         </View>
       </View>
